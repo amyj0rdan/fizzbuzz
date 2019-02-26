8b9dfb0012d7a3d0cc5a0e4bb3ede482620d8278
Lauren <laurenqurashi@me.com>
2019-02-26 13:02:30 +0000

    Added test case for negative numbers -edge case

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,4 +24,8 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 30' do
     expect(fizzbuzz(30)).to eq 'fizzbuzz'
   end
+
+  it 'returns "fizz" when passed -3' do
+    expect(fizzbuzz(-3)).to eq 'fizz'
+  end
 end
